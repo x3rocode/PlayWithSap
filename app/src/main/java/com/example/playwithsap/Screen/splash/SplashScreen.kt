562c399033c418941da4f6d8d40fc08ce7599a54
x3rocode <z3rocode@naver.com>
2023-04-27 17:11:04 +0900

    emit 수정하기

--- app/src/main/java/com/example/playwithsap/Screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/playwithsap/Screen/splash/SplashScreen.kt
@@ -3,14 +3,12 @@ import android.annotation.SuppressLint
 import android.util.Log
 import android.view.animation.LinearInterpolator
 import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,19 +32,26 @@ enum class Companies{
 @ExperimentalAnimationApi
 @Composable
 fun SplashScreen(mainViewModel: SplashViewModel = viewModel()) {
-    val companies = mainViewModel.getCompanies()
-    val company = MutableStateFlow("")
     var linearInterpolator = LinearInterpolator()
-    
-    LaunchedEffect(Unit){
-        companies.withIndex().onEach {
-            val fraction = it.index / 4f
-            val duration = 1000L
-            delay((linearInterpolator.getInterpolation(fraction) * duration).toLong())
-        }.collect{
-            company.value = it.value
-            Log.d("gggggggggggg", it.value)
+    val animatedFloat = remember { Animatable(0f) }
+    val comp = mainViewModel.companies
+    val companies =  flow {
+        mainViewModel.companies.forEachIndexed { index, indexedValue ->
+            var fraction = (index / mainViewModel.companies.size.toFloat())
+            var duration = 1000L
+            val delaymillies = (linearInterpolator.getInterpolation(fraction) * duration).toLong()
+            emit(indexedValue)
+            delay(2500L + delaymillies)
+            Log.d("dddddddddd", "애밋 ${index}, ${mainViewModel.companies.size}  ${fraction}, ${linearInterpolator.getInterpolation(fraction)}, ${index / mainViewModel.companies.size}")
         }
+
+    }
+    val company by companies.collectAsState(initial = "")
+    LaunchedEffect(animatedFloat) {
+        animatedFloat.animateTo(
+            targetValue = 8000f,
+            animationSpec = tween(delayMillis = 1000, durationMillis = 5000, easing = LinearEasing)
+        )
     }
 
 
@@ -74,13 +79,15 @@ fun SplashScreen(mainViewModel: SplashViewModel = viewModel()) {
                     style = Typography.h3
                 )
                 AnimatedContent(
-                    targetState = company.collectAsState(),
+                    targetState = company,
                     transitionSpec = {
-                        addAnimation()
+                        slideInVertically(animationSpec = tween(durationMillis = 1000)) { height -> height }  with
+                                slideOutVertically(animationSpec = tween(durationMillis = 1000 )) { height -> -height }
+
                     }
                 ) { targetCount ->
                     Text(
-                        text = "${targetCount.value}",
+                        text = "${targetCount}",
                         style = Typography.h3,
                         modifier = Modifier
                             .animateContentSize()
@@ -88,7 +95,6 @@ fun SplashScreen(mainViewModel: SplashViewModel = viewModel()) {
                     )
 
                 }
-
 
             }
         }
@@ -108,14 +114,15 @@ class SplashViewModel : ViewModel() {
     private val delayFactor = 0.3f // 지연 시간 증가율
     private var index = 1
     var delayMillis = 0f
-    val seconds =  mutableListOf("홀딩스", "케미칼", "DX", "이앤씨", "엠텍", "플로우", "A&C", "퓨처엠", "스틸리온", "인터내셔널")
+    val companies =
+        mutableListOf("홀딩스", "케미칼", "DX", "이앤씨", "엠텍", "플로우", "A&C", "퓨처엠", "스틸리온", "인터내셔널")
 
-    fun getCompanies(): Flow<String> = flow{
-        seconds.forEach{
+    fun getCompanies(): Flow<String> = flow {
+        companies.forEach {
             emit(it)
         }
     }
-
+}
 //        .asSequence()
 //        .asFlow()
 //        .onEach {
@@ -124,5 +131,5 @@ class SplashViewModel : ViewModel() {
 //            index += 1
 //            delay(delayMillis.toLong())
 //        }
-}
+//}
 
